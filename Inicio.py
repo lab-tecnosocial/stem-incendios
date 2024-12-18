b3b52267d5751e779af3f8d30c95967b034ff6de
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2024-12-17 20:16:05 -0400

    reducir variables y footer

--- Inicio.py
+++ Inicio.py
@@ -4,13 +4,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingClassifier
 from user_input import user_input
 from streamlit_extras.stylable_container import stylable_container
+from PIL import Image
 
+logoFesa = Image.open("img/logo-fesa.png")
 
 st.set_page_config(page_title="Riesgo de Incendio",
                    page_icon="🔥")
 
 if 'data' not in st.session_state:
-    st.session_state.data = pd.read_csv("Data_clima_Clasificaction.csv")
+    st.session_state.data = pd.read_csv("data_11.csv")
 
 st.title("🔥 Predicción de riesgo de incendio en Cochabamba 🔥")
 
@@ -28,15 +30,19 @@ col1, col2 = st.columns(2)
 
 data = st.session_state.data
 
+# Limpiar los nombres de las columnas
+data.columns = data.columns.str.strip()
+
+
 with col1:
     st.subheader("Ingresar datos climáticos")
-    with st.container(height=500):
+    with st.container(height=440):
         dataframe = user_input()
 
 # Entrenar el modelo al cargar la página
 if 'model' not in st.session_state:
     # Split the data into features (X) and target (y)
-    X = data.drop('Incendio', axis=1)
+    X = data.drop(['Incendio'], axis=1)
     y = data['Incendio']
 
     # Split the data into training and test sets
@@ -52,6 +58,11 @@ if 'model' not in st.session_state:
     st.session_state.X_test = X_test
     st.session_state.y_test = y_test
     st.session_state.predictions = gb_clf.predict(X_test)
+
+    # Calculate feature importances
+    feature_importances = pd.Series(
+        gb_clf.feature_importances_, index=X.columns)
+    st.session_state.feature_importances = feature_importances
 
 # Botón para ejecutar el modelo
 if "predict" not in st.session_state:
@@ -90,25 +101,36 @@ with col2:
     # button with callback
     st.button("Predecir", on_click=predict_callback, type="secondary")
 
-for i in range(10):
-    st.write("")
+footer = """<style>
 
-
-st.markdown("""
-<style>
-.big-font {
-    position: absolute !important;
-    font-size:13px !important;
-    line-height:5px !important;
-    right:100px !important;
+.footer {
+display: flex;
+justify-content: space-between;
+padding: 10px;
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: #F0F2F6;
+color: black;
+text-align: center;
+font-size: 12px;
 }
 </style>
-""", unsafe_allow_html=True)
-st.markdown('<p class="big-font">INFO</p>', unsafe_allow_html=True)
-st.markdown('<p class="big-font">Olimpiadas STEM+ 2023</p>',
-            unsafe_allow_html=True)
-st.markdown('<p class="big-font">Grupo: TecnoBanda</p>',
-            unsafe_allow_html=True)
-st.markdown('<p class="big-font">Colegio San Agustín</p>',
-            unsafe_allow_html=True)
-st.markdown('<p class="big-font">CBBA - Bolivia</p>', unsafe_allow_html=True)
+<div class="footer">
+    <div></div>
+    <div>
+        <div><b>Desarrollado por:</b></div>
+        <div>Equipo Ganador Olimpiadas STEM+ 2024 <img src="https://fesa.edu.bo/wp-content/uploads/2024/04/STEM-logo-color-3.png" width="50px"></div>
+        <div>TecnoBanda, Colegio San Agustín</div>
+    </div>
+    <div>
+    <div><b>Con el Apoyo de:</b></div>
+        <a href="https://fesa.edu.bo/"><img src="https://fesa.edu.bo/wp-content/uploads/2024/05/Logo-FESA.png" width="50px" alt="Logo" style="margin-right: 10px;"></a>
+        <a href="https://labtecnosocial.org/"><img src="https://labtecnosocial.org/wp-content/uploads/2021/07/cropped-logo-claro-300x149.png" width="70px" alt="Logo"></a>
+    </div>
+    <div></div>
+
+</div>
+"""
+st.markdown(footer, unsafe_allow_html=True)
